03e0ed31923993c1a3d1a9b869a1f3ae2a2b224f
Денис Братчиков <ds.bratchikov@tensor.ru>
2023-02-24 13:23:07 +0100

    fix hover bugs

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,36 +1,38 @@
-import cn from "classnames";
+import cn from 'classnames';
 
-import { getWithDivider } from "../../utils";
+import { getWithDivider } from '../../utils';
 
-import style from "./style.module.css";
+import style from './style.module.css';
 
 export default function List({
   title,
   items,
   noItemsMessage,
-  selectedRowId,
-  onSelectRow,
+  selectedRow,
+  onSelectRow
 }) {
   return (
     <div className={style.container}>
       <h3>{title}</h3>
       {items.length ? (
         <ul className={style.list}>
-          {items.map((item) => (
-            <li
-              key={item.id}
-              className={cn(style.listItem, {
-                [style.selectable]: !!onSelectRow,
-                [style.selectedListItem]: selectedRowId === item.id,
-              })}
-              onClick={() => onSelectRow(item)}
-            >
-              {`${getWithDivider(
-                item.homeTeam,
-                item.awayTeam
-              )}: ${getWithDivider(item.homeScore, item.awayScore)}`}
-            </li>
-          ))}
+          {items.map((item) => {
+            const isSelected = selectedRow && selectedRow.id === item.id;
+            return (
+              <li
+                key={item.id}
+                className={cn(style.listItem, {
+                  [style.selectable]: !!onSelectRow,
+                  [style.selectedListItem]: isSelected
+                })}
+                onClick={() => onSelectRow(isSelected ? null : item)}>
+                {`${getWithDivider(
+                  item.homeTeam,
+                  item.awayTeam
+                )}: ${getWithDivider(item.homeScore, item.awayScore)}`}
+              </li>
+            );
+          })}
         </ul>
       ) : (
         <div className={style.noItems}>{noItemsMessage}</div>
